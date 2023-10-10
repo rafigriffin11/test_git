26378299c30726f39368b0230e1d5b29be5a6527
unknown <rafigriffin11@gmail.com>
2023-10-10 09:06:57 -0600

    first commit

---
+++ test.py
@@ -0,0 +1,7 @@
+# git = version control software, allows developers to track changes in their code
+# github = utilizes software, developers can modify eachothers code.
+#                    --think linkedin for coding
+
+
+
+print('hello world!')
